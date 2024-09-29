3431a86505fe256761f3204913b0824c00b92905
gayratovdv <javohirgayratov26@gmail.com>
2024-09-29 11:26:39 +0500

    something

---
+++ src/pages/NotFound/notfound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./notfound.css"
+
+const NotFoundPage = () => {
+  return (
+    <div className="notfound">
+      <h1>404 - Not Found Page</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
