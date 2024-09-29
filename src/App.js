b54badcbd9606bf3a275b42531189eb7a13721a0
gayratovdv <javohirgayratov26@gmail.com>
2024-09-29 09:19:27 +0500

    create folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/about' element={<h1>About</h1>} />
+      <Route path='/contact' element={<h1>Contact</h1>} />
+    </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
